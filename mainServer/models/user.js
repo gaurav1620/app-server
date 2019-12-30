ca65d74cead63248e5ee4f251764f5df59a91513
gaurav1620 <gauravak007@gmail.com>
2019-12-30 17:07:23 +0530

    successfully added mongo db

---
+++ mainServer/models/user.js
@@ -0,0 +1,33 @@
+let mongoose = require('mongoose')
+
+let User = mongoose.model('User',new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLength:2,
+        trim:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        minLength:2,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        minLength:2,
+        trim:true,
+    },
+    phone:{
+        type:String,
+        required:false,
+        minLength:10,
+        maxLength:10,
+    },
+    pass:{
+        type:String,
+        
+    }
+}))
+module.exports = {User}
