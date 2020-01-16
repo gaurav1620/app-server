6448ffbcff9b6a9b683e3de3ebcfdcd3df3c45ab
gaurav1620 <gauravak007@gmail.com>
2020-01-16 23:03:42 +0530

    added mlab and chaged mongo server to process .env and added dotnev in server js line 1

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 const dbName = 'Sih-Fsociety'
 
 mongoose.Promise = global.Promise
-mongoose.connect(`mongodb://localhost:27017/${dbName}`,
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`,
     {
         useNewUrlParser:true,
         useUnifiedTopology:true,
